ce0562085a321cbb19e48d37eec1a2ab200eafc7
VNeskoromnaya <neskoromnaya.v@gmail.com>
2022-02-07 17:39:07 +0200

    update html css js

---
+++ index.js
@@ -0,0 +1,25 @@
+// debugger
+
+let wrapper = document.getElementById('wrapper');
+let color = document.getElementById('color');
+
+
+function changeBackground() {
+    if(color.value == "white") {
+        wrapper.style.background = 'white';
+    }
+    if(color.value == "green") {
+    wrapper.style.background = 'green';
+    }
+    if(color.value == "orange") {
+        wrapper.style.background = 'orange';
+    }
+    if(color.value == "yellow") {
+        wrapper.style.background = 'yellow';
+    }
+    if(color.value == "pink") {
+        wrapper.style.background = 'pink';
+    }
+}
+
+color.onchange = changeBackground;
